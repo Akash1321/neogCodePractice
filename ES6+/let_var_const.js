d71623fa0485e283a7308fa8fa17ad2ce5182a89
Akash1321 <Sarkarakash992@gmail.com>
2023-02-13 10:38:10 +0530

    practice done

---
+++ ES6+/let_var_const.js
@@ -0,0 +1,62 @@
+// First
+// if(true){
+//   // let a = 2; 
+//     var a = 2; 
+// }
+
+// console.log(a);
+
+//second
+
+// let a = 42;
+// {
+//     let a = 100;
+// }
+
+// console.log(a);
+
+// third
+
+// let a = 100;
+// function App(){
+
+    
+//     console.log("1", a);
+//     let a  = 42;
+//     console.log("2", a);
+//     {
+//         let a = 100;
+//     }
+//     console.log("3", a)
+// }
+
+// App()
+
+
+//fourth
+
+// let a = 100;
+// function App() {
+//   a = 42;
+// 	console.log("1",a);
+// }
+
+// App()
+// console.log("final", a)
+
+//fifth 
+
+// const obj = {a: 1, b:2};
+
+// const obj1 = {a: 4, b: 7};
+
+// obj = obj1;
+
+//sixth 
+const arr = [1, 2, 3, 4, 5];
+arr.push(7)
+
+console.log(arr.push());
+
+
+
